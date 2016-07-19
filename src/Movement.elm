3876c8949b9fe2c4facff8791864534721b83707
kongus99 <kongus99@gmail.com>
2016-07-19 15:10:00 +0200

    cleanup

--- src/Movement.elm
+++ src/Movement.elm
@@ -11,6 +11,9 @@ import Json.Decode as Json exposing ((:=))
 import Maybe exposing (withDefault)
 import BoardData exposing (..)
 import Paths exposing (..)
+import Graphics exposing (..)
+
+
 -- MODEL
 type alias Color = String
 type alias Model = { start : (Place Neighborhood Location), path : List (Place Neighborhood Location), movementPoints : Int, isValid : Bool }
@@ -79,7 +82,7 @@ merge path1 path2 =
 
 view model = svg [ width "1606", height "2384" ] (concat
                                                 [ [boardImage]
-                                                , (map locationCircle allLocation)
+                                                , (map localeCircle allLocation)
                                                 , (map streetRectangle allNeighborhood)
                                                 , (movementLines model)
                                                 , [positionCircle model.start True]
@@ -88,53 +91,16 @@ view model = svg [ width "1606", height "2384" ] (concat
 boardImage =
   image [xlinkHref "board.jpg", x "0", y "0", width "1606", height "2384", on "click" (Json.map Show offsetPosition)][]
 
-type alias Point = {x : Int, y : Int}
-type alias LineConnective shape = { shape | connector : Point }
 --Location circles
-type alias Circle = LineConnective {cx : Int, cy : Int, radius : Int}
 
-locationCircle : Location -> Svg Msg
-locationCircle l =
-    circle (circleParams (locationDimensions l) l) []
-
-locationDimensions location =
-    let
-        circleMiddle c = {x = c.cx, y = c.cy}
-        loc =
-            case location of
-                 Train_Station ->          {cx = 264,  cy = 112,  radius = 64}
-                 Independence_Square ->    {cx = 924,  cy = 112,  radius = 64}
-                 Bank_of_Arkham ->         {cx = 487,  cy = 112,  radius = 64}
-                 Arkham_Asylum ->          {cx = 706,  cy = 112,  radius = 64}
-                 Newspaper ->              {cx = 126,  cy = 286,  radius = 64}
-                 Hibb's_Roadhouse ->       {cx = 927,  cy = 356,  radius = 64}
-                 Velma's_Diner ->          {cx = 1148, cy = 351,  radius = 64}
-                 Curiositie_Shoppe ->      {cx = 125 , cy = 473,  radius = 64}
-                 Unvisited_Isle ->         {cx = 124 , cy = 759,  radius = 64}
-                 Police_Station ->         {cx = 1148, cy = 551,  radius = 64}
-                 Graveyard ->              {cx = 1148, cy = 845,  radius = 64}
-                 River_Docks ->            {cx = 122,  cy = 938,  radius = 64}
-                 The_Unnamable ->          {cx = 342,  cy = 1025, radius = 64}
-                 General_Store ->          {cx = 898,  cy = 1017, radius = 64}
-                 Black_Cave ->             {cx = 1150, cy = 1030, radius = 64}
-                 Science_Building ->       {cx = 122,  cy = 1195, radius = 64}
-                 The_Witch_House ->        {cx = 1150, cy = 1312, radius = 64}
-                 Silver_Twilight_Lodge ->  {cx = 936,  cy = 1383, radius = 64}
-                 Library ->                {cx = 447,  cy = 1382, radius = 64}
-                 Administration_Building ->{cx = 243,  cy = 1450, radius = 64}
-                 St_Mary's_Hospital ->     {cx = 122,  cy = 1701, radius = 64}
-                 Ma's_Boarding_House ->    {cx = 1149, cy = 1640, radius = 64}
-                 South_Church ->           {cx = 1058, cy = 1901, radius = 64}
-                 Historical_Society ->     {cx = 816,  cy = 1976, radius = 64}
-                 Woods ->                  {cx = 562,  cy = 1976, radius = 64}
-                 Ye_Olde_Magick_Shoppe ->  {cx = 282,  cy = 1947, radius = 64}
-                 _ ->                      {cx = 0,    cy = 0,    radius = 0}
-    in
-        {cx = loc.cx, cy = loc.cy, radius = loc.radius, connector = circleMiddle loc}
+localeCircle : Location -> Svg Msg
+localeCircle l =
+    circle (circleParams (locationCircle l) l) []
 
 circleParams c l =
     [cx <| toString c.cx, cy <| toString c.cy, r <| toString c.radius, strokeWidth "1", strokeOpacity "0.0", fillOpacity "0.0", onDoubleClick Submit, onClick <| Move (Locale l)]
--- Position circle
+
+-- Position circles
 positionCircle : Place Neighborhood Location -> Bool -> Svg a
 positionCircle p isFilled =
     let
@@ -143,28 +109,9 @@ positionCircle p isFilled =
         circle [cx <| toString connector.x, cy <| toString connector.y, r "12", strokeWidth "3", stroke "black", strokeOpacity "1.0", fillOpacity (if isFilled then "1.0" else "0.0")][]
 
 -- Street rectangles
-type alias Rectangle = LineConnective {x : Int, y : Int, width : Int, height : Int}
 
 streetRectangle s =
-    rect (rectangleParams (streetDimensions s) s) []
-
-streetDimensions street =
-    let
-        rectangleMiddle r =
-            {x = floor <| r.x + (r.width / 2), y = floor <| r.y + (r.height / 2)}
-        st =
-           case street of
-                Northside ->            {x = 296, y = 396,  width = 180, height = 80}
-                Downtown ->             {x = 591, y = 393,  width = 192, height = 82}
-                Easttown ->             {x = 731, y = 621,  width = 208, height = 82}
-                Merchant_District ->    {x = 363, y = 808,  width = 264, height = 80}
-                Rivertown ->            {x = 720, y = 808,  width = 182, height = 78}
-                Miskatonic_University ->{x = 379, y = 1177, width = 236, height = 84}
-                French_Hill ->          {x = 721, y = 1219, width = 195, height = 84}
-                Uptown ->               {x = 469, y = 1669, width = 152, height = 84}
-                Southside ->            {x = 753, y = 1673, width = 180, height = 82}
-    in
-        {x = st.x, y = st.y,  width = st.width, height = st.height, connector = rectangleMiddle st}
+    rect (rectangleParams (neighborhoodRectangle s) s) []
 
 rectangleParams r s =
     [ x <| toString r.x, y <| toString r.y, width <| toString r.width, height <| toString r.height, strokeOpacity "0.0", fillOpacity "0.0", onDoubleClick Submit, onClick <| Move (Street s)]
@@ -180,8 +127,8 @@ movementLines model=
 
 connectorPoint place =
     case place of
-        Street s -> (streetDimensions s).connector
-        Locale l -> (locationDimensions l).connector
+        Street s -> (neighborhoodRectangle s).connector
+        Locale l -> (locationCircle l).connector
 
 movement: Color -> (Place Neighborhood Location, Place Neighborhood Location) -> Svg a
 movement color (start, end) = drawLine (connectorPoint start) (connectorPoint end) color
