3e0db0828abe3fdd50dec5cc72c1b1ffe13e2277
kongus99 <kongus99@gmail.com>
2016-07-19 14:29:31 +0200

    revert

--- src/boardData.elm
+++ src/boardData.elm
@@ -2,7 +2,7 @@ module BoardData exposing (..)
 
 type Neighborhood = Downtown | Easttown | French_Hill |
                     Merchant_District | Miskatonic_University | Northside |
-                    Rivertown | Southside | Uptown | XXX
+                    Rivertown | Southside | Uptown
 allNeighborhood = [Downtown, Easttown, French_Hill, Merchant_District, Miskatonic_University, Northside, Rivertown, Southside, Uptown]
 
 streetRectangle street =
