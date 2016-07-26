128c509e476964748013dfae564f5d20d96027a3
kongus99 <kongus99@gmail.com>
2016-07-26 14:40:30 +0200

    tests now work for multiple monsters on path

--- src/DiceTester.elm
+++ src/DiceTester.elm
@@ -8,6 +8,7 @@ import Html.Events exposing (onClick)
 import Html.Attributes exposing (style)
 import Random
 import Task
+import String
 
 type alias TestResult = Bool
 type alias NumOfTests = Int
@@ -16,30 +17,25 @@ type alias DiceThrowResult = Int
 type alias NumOfSuccesses = Int
 -- MODEL
 
-type alias Model = { rolls : List Dice.Model, isSuccess : TestResult}
-initialModel availableDices =
-    { rolls = List.repeat availableDices Dice.initialModel, isSuccess = False }
+type alias Model = { rolls : List Dice.Model, isSuccess : TestResult, testName : String}
+initialModel availableDices name =
+    { rolls = List.repeat availableDices Dice.initialModel, isSuccess = False, testName = name }
+
+createModel name threshold required newRolls =
+    let
+        rollSuccessful res = res >= threshold
+        isSuccess = (List.length (List.filter rollSuccessful newRolls)) >= required
+        createNewRoll res =
+            Dice.update (Dice.NewFace res (rollSuccessful res)) Dice.initialModel
+    in
+        {rolls = List.map createNewRoll newRolls, isSuccess = isSuccess, testName = name }
 
 -- UPDATE
 runTest : NumOfTests -> (List Int -> a) -> Cmd a
 runTest numOfTests wrapper =
     Random.generate wrapper <| Random.list numOfTests (Random.int 1 6)
 
-type Msg = SetNewResult SuccessThreshold NumOfSuccesses (List DiceThrowResult)
-
-update : Msg -> Model -> Model
-update message model =
-  case message of
-    SetNewResult threshold required newRolls ->
-        let
-            rollSuccessful res = res >= threshold
-            isSuccess = (List.length (List.filter rollSuccessful newRolls)) >= required
-            createNewRoll res =
-                Dice.update (Dice.NewFace res (rollSuccessful res)) Dice.initialModel
-        in
-            {model | rolls = List.map createNewRoll newRolls, isSuccess = isSuccess}
-
 -- VIEW
 view : Model -> Html a
 view model = div[][ div[style [("display", "flex"),("flex-flow", "row wrap"),("justify-content", "space-around")]] (List.map Dice.view model.rolls)
-                  , span[][text <| toString model.isSuccess]]
+                  , span[][text <| String.append model.testName <| toString model.isSuccess]]
--- src/Game.elm
+++ src/Game.elm
@@ -4,17 +4,21 @@ import Html exposing (Html, button, div, text)
 import Html.App as App
 import Html.Events exposing (onClick)
 import Array exposing (Array)
-import MonsterBowl
+import MonsterBowl exposing (Monster)
 import AllDict
+import List.Extra exposing (zip)
+import BoardData exposing(..)
 -- MODEL
 
-type alias Model = { board : Movement.Model, tester : DiceTester.Model, monsterBowl : Maybe MonsterBowl.Bowl }
+type alias TestData = {testsToPerform :Int, location : Place Neighborhood Location, testName : String, requiredSuccesses : Int}
 
-initialModel = { board = Movement.initialModel, tester = DiceTester.initialModel 0, monsterBowl = Nothing }
+type alias Model = { board : Movement.Model, testers : List DiceTester.Model, monsterBowl : Maybe MonsterBowl.Bowl }
+
+initialModel = { board = Movement.initialModel, testers = [], monsterBowl = Nothing }
 
 -- UPDATE
 
-type Msg = BoardMove Movement.Msg | SneakTest Int (List Int)
+type Msg = BoardMove Movement.Msg | SneakTest (List TestData) (List Int)
 
 update message model =
   case message of
@@ -22,12 +26,10 @@ update message model =
       case msg of
           Movement.Submit ->
             let
-                firstMonster = List.head (AllDict.toList model.board.obstructions)
-                numOfTests = case firstMonster of
-                                Just (_, monster) -> model.board.investigator.sneak - monster.awareness
-                                _ -> 0
+                tests = prepareSneakTestData model.board
+                numOfTests = List.sum <| List.map (\t -> t.testsToPerform) tests
              in
-                ({model | tester = DiceTester.initialModel numOfTests}, DiceTester.runTest numOfTests (SneakTest 1))
+                (model, DiceTester.runTest numOfTests (SneakTest tests))
           Movement.AddObstruction place _ ->
             let
                 (maybeMonster, bowl) = MonsterBowl.drawMonster model.monsterBowl
@@ -35,18 +37,37 @@ update message model =
             in
                 ({model | board = updatedBoard, monsterBowl = Just bowl}, Cmd.none)
           _ -> ({ model | board = Movement.update msg model.board }, Cmd.none)
-    SneakTest requiredSuccesses results ->
+    SneakTest tests results ->
         let
-            updateTester = DiceTester.update (DiceTester.SetNewResult 5 requiredSuccesses results) model.tester
-            newBoard = if updateTester.isSuccess then Movement.update Movement.Submit model.board else model.board
+            testsWithRolls = testsWithResults results tests
+            testers = List.map (\(res, test) -> DiceTester.createModel test.testName test.testsToPerform test.requiredSuccesses res) testsWithRolls
+            failedTests = List.filter (\t -> not t.isSuccess) testers
+            newBoard = if List.isEmpty failedTests then Movement.update Movement.Submit model.board else model.board
         in
-            ({model | tester = updateTester, board = newBoard}, Cmd.none)
+            ({model | testers = testers, board = newBoard}, Cmd.none)
+
+testsWithResults : List Int -> List TestData -> List (List Int, TestData)
+testsWithResults results testData =
+    let
+        indexes = List.scanl (+) 0 (List.map (\t -> t.testsToPerform) testData)
+        intervals = zip indexes <| Maybe.withDefault [] <| List.tail indexes
+        splitResults = List.map (\(start, end) -> List.drop start (List.take end results)) intervals
+    in
+        zip splitResults testData
+
+prepareSneakTestData : Movement.Model -> List TestData
+prepareSneakTestData model =
+    let
+        monsters = AllDict.toList model.obstructions
+        monstersOnPath = List.filter (\(p, m) -> not <| List.isEmpty model.path && List.member p (model.start :: model.path)) monsters
+    in
+        List.map (\(p, m) -> {testsToPerform =  max 0 (model.investigator.sneak - m.awareness), location = p, testName = "Sneak", requiredSuccesses= 1}) monstersOnPath
 -- VIEW
 
 view : Model -> Html Msg
 view model =
   div[][
-        div [] [DiceTester.view model.tester]
+        div [] (List.map DiceTester.view model.testers)
        ,div [] [App.map BoardMove (Movement.view model.board)]
        ]
 
