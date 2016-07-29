ea9fdeef427938dcfe6566add9c05a4bcccb9456
kongus99 <kongus99@gmail.com>
2016-07-29 10:35:12 +0200

    cleanup + resolved rolls visualization

--- src/DiceChecker.elm
+++ src/DiceChecker.elm
@@ -3,14 +3,16 @@ module DiceChecker exposing (..)
 import BoardData exposing (..)
 import String
 import Random
-import Html exposing (Html, div, h1, text)
+import Html exposing (Html, div, h1, text, span, Attribute)
 import Html.Attributes exposing (style)
 
 type CheckType = Evade
 
+type alias WasSuccess = Bool
+
 type alias CommonCheck a = {a | location : Place Neighborhood Location, checkType : CheckType, dicesAmount : Int}
 type alias DiceCheck = CommonCheck {requiredSuccesses : Int, successThreshold : Int}
-type alias ResolvedDiceCheck = CommonCheck {dices : List (Int, Bool), wasSuccess : Bool}
+type alias ResolvedDiceCheck = CommonCheck {dices : List (Int, WasSuccess), wasSuccess : WasSuccess}
 
 prepareCheck location checkType dicesAmount requiredSuccesses successThreshold =
     {location = location, checkType = checkType, dicesAmount = dicesAmount, requiredSuccesses = requiredSuccesses, successThreshold = successThreshold}
@@ -40,3 +42,21 @@ testDetails check =
 
 drawDiceCheck : DiceCheck -> Html a
 drawDiceCheck check = div [][h1[][text <| testName check.checkType], div[][text <| testDetails check]]
+
+drawResolvedDiceCheck : ResolvedDiceCheck -> Html a
+drawResolvedDiceCheck roll = div[][ div[style [("display", "flex"),("flex-flow", "row wrap"),("justify-content", "space-around")]] (List.map singleDice roll.dices)
+                                  , span[][text <| String.append (testName roll.checkType) <| toString roll.wasSuccess]]
+
+singleDice : (Int, WasSuccess) -> Html a
+singleDice (faceValue, wasSuccess) = span [diceStyle wasSuccess] [ text (toString faceValue) ]
+
+diceStyle: WasSuccess -> Attribute msg
+diceStyle wasSuccess =
+    let
+        styleArgs =
+            if wasSuccess then
+                [ ("backgroundColor", "white"), ("color", "green"), ("fontSize", "250%")]
+            else
+                [ ("backgroundColor", "white"), ("color", "red"), ("fontSize", "250%")]
+    in
+        style styleArgs
